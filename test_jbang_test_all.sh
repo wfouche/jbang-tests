41e49e8b05786a6990bdd2d8872477a7e7d11ef5
Werner Fouché <werner.fouche@gmail.com>
2025-04-02 16:29:45 +0200

    Additional test scripts

---
+++ test_jbang_test_all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pushd ~/IdeaProjects/jbang
+unset JBANG_JAVA_OPTIONS
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk env
+rm -f -r build
+rm -f -r temp
+./gradlew spotlessCheck
+./gradlew test
+popd
